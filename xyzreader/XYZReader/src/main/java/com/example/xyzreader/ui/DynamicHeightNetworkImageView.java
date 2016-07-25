0fffc934329483003faec741f471bad39691d555
fanr1r <pk@fanrir.com>
2016-07-25 21:19:22 +0200

    fix: aspect ratio for custom ImageView

--- xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ xyzreader/XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -2,10 +2,11 @@ package com.example.xyzreader.ui;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 
 import com.android.volley.toolbox.NetworkImageView;
 
-public class DynamicHeightNetworkImageView extends NetworkImageView {
+public class DynamicHeightNetworkImageView extends ImageView {
     private float mAspectRatio = 1.5f;
 
     public DynamicHeightNetworkImageView(Context context) {
@@ -27,8 +28,13 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        int measuredWidth = getMeasuredWidth();
-        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+        int aspectRatioHeight;
+        if (mAspectRatio < 0) {
+            aspectRatioHeight = (int) (MeasureSpec.getSize(widthMeasureSpec) * mAspectRatio);
+        } else {
+            aspectRatioHeight = (int) (MeasureSpec.getSize(widthMeasureSpec) * (1/mAspectRatio));
+        }
+        int aspectRatioHeightSpec = MeasureSpec.makeMeasureSpec(aspectRatioHeight, MeasureSpec.EXACTLY);
+        super.onMeasure(widthMeasureSpec, aspectRatioHeightSpec);
     }
 }
